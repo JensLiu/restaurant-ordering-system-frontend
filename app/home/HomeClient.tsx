3bad8aed1f5adb6317d51237a1d290862997d87f
Jens Liu <jensliu39@gmail.com>
2023-06-25 15:23:00 +0800

    minor fixes

--- app/home/HomeClient.tsx
+++ app/home/HomeClient.tsx
@@ -19,9 +19,6 @@ const HomeClient: FC<HomeClientProps> = ({ menuItems }) => {
 
     return (
         <div className="container mx-auto py-5">
-            {useUserStore.getState().accessToken}
-            <br />
-            {useUserStore.getState().refreshToken}
             <h1 className="text-3xl font-bold mb-3">Our finest cuisines</h1>
             <h1 className="text-md font-medium mb-6">Our menu at a glance</h1>
             {/* {cart.items.map((item) => (<div>{JSON.stringify(item)}</div>))} */}

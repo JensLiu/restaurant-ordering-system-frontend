5c19533ada4424b4113bb50f34a563d065c0d082
Jens Liu <jensliu39@gmail.com>
2023-06-27 08:02:53 +0800

    Added no-cache header to fetch dynamic data in ssr

--- app/actions/axios.ts
+++ app/actions/axios.ts
@@ -14,11 +14,19 @@ export const apiBaseUrl = `http${
 // client side instance that needs auth header and can refresh token on 401
 const axiosInstance = axios.create({
     baseURL: apiBaseUrl,
+    // Solution provided by PedroTech at https://www.youtube.com/watch?v=8pzIuLFuv6U&ab_channel=PedroTech
+    // for fetching dynamic data from the server
+    headers: {
+        "Cache-Control": "no-cache",
+    }
 });
 
 // public instance does not need auth header, and can be used in server side rendering
 export const axiosPublicInstance = axios.create({
     baseURL: apiBaseUrl,
+    headers: {
+        "Cache-Control": "no-cache",
+    },
 });
 
 const unwrapDto = (response: AxiosResponse) => {
